ef2a01f764372e9eb62c1a69ea477657c08d0d1e
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-08-07 20:18:35 +0100

    refactoring script.js to use the new es6 syntaxes

--- script.js
+++ script.js
@@ -1,52 +1,58 @@
-var data = [
+// data array
+let data = [
   {
-    "Cheese": 22.2,
-    "CHOCOLATE": 10.3,
-    "Impulse": 1.5,
-    "period": "2021_26"
+    Cheese: 22.2,
+    CHOCOLATE: 10.3,
+    Impulse: 1.5,
+    period: "2021_26"
   },
   {
-    "Cheese": 21.8,
-    "CHOCOLATE": 9.8,
-    "Impulse": 1.5,
-    "period": "2021_27"
+    Cheese: 21.8,
+    CHOCOLATE: 9.8,
+    Impulse: 1.5,
+    period: "2021_27"
   },
   {
-    "Cheese": 21.2,
-    "CHOCOLATE": 9.7,
-    "Impulse": 1.4,
-    "period": "2021_28"
+    Cheese: 21.2,
+    CHOCOLATE: 9.7,
+    Impulse: 1.4,
+    period: "2021_28"
   }
 ];
 
-function generateGraph() {
-  var dataWithTotal = [];
+/**
+ * @function generateGraph
+ * @description This function generates the graph.
+ */
+const generateGraph = () => {
+  let dataWithTotal = [];
   
-  for (var i = 0; i < data.length; i++) {
-    var temp = data[i];
-    total = 0;
-    for (var key in data[i]) {
-      if(key !== "period") {
-        total += data[i][key];
+  data.forEach(d => {
+    let total = 0;
+    for (let prop in d) {
+      if (prop !== "period") {
+        total += d[prop];
       }
     }
-    temp.total = total / 3;
-    dataWithTotal.push(temp);
-  }
+    dataWithTotal.push({
+      period: d.period,
+      total: total / data.length
+    });
+   });
   
-  var labels = [];
+  let labels = [];
+
+  dataWithTotal.forEach(d => {
+    labels.push(d.period);
+  });
   
-  for (var i = 0; i < dataWithTotal.length; i++) {
-    labels.push(dataWithTotal[i]['period']);
-  }
-  
-  var graphValues = [];
-  var keys = Object.keys(dataWithTotal[0]);
-  
-  for (var i = 0; i < keys.length; i++) {
-    if(keys[i] !== "period") {
+  let graphValues = [];
+  let keys = Object.keys(dataWithTotal[0]);
+
+  keys.forEach(key => {
+    if (key !== "period") {
       var temp = {
-        label: keys[i], 
+        label: keys[i],
         data: [],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
@@ -59,19 +65,17 @@ function generateGraph() {
           'rgba(54, 162, 235, 1)',
           'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)'
-        ], 
+        ],
       };
-  
-      for (var n = 0; n < dataWithTotal.length; n++) {
-        temp.data.push(dataWithTotal[n][keys[i]])
-      }
-      
+      dataWithTotal.forEach(d => {
+        temp.data.push(d[key]);
+        });
       graphValues.push(temp);
     }
-  }
+  });
   
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let myChart = new Chart(ctx, {
     type: 'line',
     data: {
       labels: labels,
